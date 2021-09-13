ea95771c5a7bacb89898f865f412fec8117833a4
JHBonarius <36847155+JHBonarius@users.noreply.github.com>
2021-09-13 16:07:47 +0200

    forgot main input

--- HelloWorld.cxx
+++ HelloWorld.cxx
@@ -1,7 +1,7 @@
 #include "HelloWorldConfig.h" // generated file
 #include <iostream>
 
-int main() {
+int main(int argc, char* argv[]) {
   // report version
   std::cout << argv[0] << " Version " << HelloWorld_VERSION_MAJOR << "."
             << HelloWorld_VERSION_MINOR << '\n\n';
